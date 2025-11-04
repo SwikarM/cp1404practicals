59d837dee93e277a997625da569ab1a4717ec267
maina <mainaliswikar@gmail.com>
2025-11-04 14:32:21 +1000

    Add prac 1 files

---
+++ prac_06/language.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 Practical - ProgrammingLanguage class.
+Estimate: 25 minutes
+Actual: 45 minutes
+"""
+
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
